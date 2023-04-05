d244b4aace37a594675b24f70a5c4290357fe164
Roger Twan <roger.twan@gmail.com>
2023-04-06 00:31:23 +0800

    :triangular_flag_on_post: Finish Lyric screen

--- lib/ui/lyric/index.dart
+++ lib/ui/lyric/index.dart
@@ -20,7 +20,8 @@ class _LyricScreenState extends State<LyricScreen> {
       baseFontSize = minFontSize;
     }
 
-    String lyr = '[ti:一个人的北京]\n[ar:好妹妹乐队]\n[al:南北]\n[by:]\n[offset:0]\n[00:00.10]一个人的北京 - 好妹妹乐队\n[00:00.20]词：秦昊\n[00:00.30]曲：秦昊\n[00:00.40]\n[00:30.16]你有多久没有看到 满天的繁星\n[00:37.34]城市夜晚虚伪的光明 遮住你的眼睛\n[00:44.40]连周末的电影 也变得不再有趣\n[00:51.71]疲惫的日子里 有太多的问题\n[00:59.21]\n[01:00.96]你有多久单身一人 不再去旅行\n[01:08.20]习惯下班回到家里 冷冰冰的空气\n[01:15.58]爱情这东西 你已经不再有勇气\n[01:22.64]情歌有多动听 你就有多怀疑\n[01:30.60]许多人来来去去 相聚又别离\n[01:38.29]也有人喝醉哭泣 在一个人的北京\n[01:45.16]也许我成功失意 慢慢的老去\n[01:52.76]能不能让我留下片刻的回忆\n[01:58.95]\n[04:34.24]也有人匆匆逃离 这一个人的北京\n[04:41.37]也许有一天我们 一起离开这里\n[04:48.87]离开了这里 在晴朗的天气\n[04:55.08]';
+    String lyr =
+        '[00:30.16]你有多久没有看到 满天的繁星\n[00:37.34]城市夜晚虚伪的光明 遮住你的眼睛\n[00:44.40]连周末的电影 也变得不再有趣\n[00:51.71]疲惫的日子里 有太多的问题\n[01:00.96]你有多久单身一人 不再去旅行\n[01:08.20]习惯下班回到家里 冷冰冰的空气\n[01:15.58]爱情这东西 你已经不再有勇气\n[01:22.64]情歌有多动听 你就有多怀疑\n[01:30.60]许多人来来去去 相聚又别离\n[01:38.29]也有人喝醉哭泣 在一个人的北京\n[01:45.16]也许我成功失意 慢慢的老去\n[01:52.76]能不能让我留下片刻的回忆\n[01:58.95]\n[04:34.24]也有人匆匆逃离 这一个人的北京\n[04:41.37]也许有一天我们 一起离开这里\n[04:48.87]离开了这里 在晴朗的天气\n[04:55.08]';
 
     return Container(
       padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
@@ -29,25 +30,30 @@ class _LyricScreenState extends State<LyricScreen> {
       width: double.infinity,
       child: Column(
         children: [
-          Text(
-            'Hotel California',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            fontSize: baseFontSize,
-            color: Colors.white,
-          )),
-          Text(
-            'Eagle',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            fontSize: baseFontSize * 0.6,
-          )),
-          Expanded(child: Container(
-            padding: const EdgeInsets.only(top: 10),
+          Text('Hotel California',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: baseFontSize,
+                color: Colors.white,
+              )),
+          Text('Eagle',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: baseFontSize * 0.6,
+              )),
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.only(top: 50),
             width: double.infinity,
             child: CustomPaint(
               painter: LyricWrapper(lyr, baseFontSize),
             ),
+            // child: Column(
+            //   children: [
+            //     const SizedBox(height: 100),
+            //     CircularProgressIndicator(color: Colors.grey[800]),
+            //   ],
+            // ),
           ))
         ],
       ),
--- lib/ui/lyric/lyric_wrapper.dart
+++ lib/ui/lyric/lyric_wrapper.dart
@@ -10,26 +10,46 @@ class LyricWrapper extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     List<Lyric> lyric = formatLyric(lyricStr);
     double y = 0;
+    int currentIndex = 4;
+
+    TextStyle commonStyle = TextStyle(
+      color: Colors.grey[700],
+      fontSize: baseFontSize * 0.4,
+    );
+    TextStyle primaryStyle = TextStyle(
+      color: Colors.grey[200],
+      fontSize: baseFontSize * 0.8,
+    );
+    TextStyle secondaryStyle = TextStyle(
+      color: Colors.grey[400],
+      fontSize: baseFontSize * 0.6,
+    );
+    TextStyle tertiaryStyle = TextStyle(
+      color: Colors.grey[700],
+      fontSize: baseFontSize * 0.5,
+    );
 
     for (int i = 0; i < lyric.length; i++) {
+      TextStyle lyricStyle = commonStyle;
+
+      if (isWithinIndex(i, currentIndex, 0)) {
+        lyricStyle = primaryStyle;
+      } else if (isWithinIndex(i, currentIndex, 1)) {
+        lyricStyle = secondaryStyle;
+      } else if (isWithinIndex(i, currentIndex, 2)) {
+        lyricStyle = tertiaryStyle;
+      }
+
       TextPainter textPainter = TextPainter(
         text: TextSpan(
           text: lyric[i].lyric,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 40,
-          )
+          style: lyricStyle,
         ),
         textDirection: TextDirection.ltr,
       );
       textPainter.layout();
 
-      if (i == 0) {
-        // y = size.height - (size.height - textPainter.height) / 2;
-        y = 0;
-      } else {
-        y += 30;
-      }
+      y = i == 0 ? 0 : y + baseFontSize * 1.5;
 
       final xCenter = (size.width - textPainter.width) / 2;
       final offset = Offset(xCenter, y);
@@ -39,4 +59,8 @@ class LyricWrapper extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => false;
+
+  bool isWithinIndex(int index, int baseIndex, int range) {
+    return (index == baseIndex + range) || (index == baseIndex - range);
+  }
 }
