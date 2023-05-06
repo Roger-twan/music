d865fbd0f7a3d9f5ec35c0b66c3887bf924549b4
Roger Twan <roger.twan@gmail.com>
2023-05-07 01:04:49 +0800

    :wrench: Add configuration file

--- lib/provider/dio_client.dart
+++ lib/provider/dio_client.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
+import '../config.dart';
 
 Dio dioClient() {
   Dio dio = Dio();
   Logger logger = Logger();
 
-  dio.options.baseUrl = 'https://music.twan.life';
+  dio.options.baseUrl = Config().apiHost;
 
   dio.interceptors.add(
     InterceptorsWrapper(
